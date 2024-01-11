dd12ddce48eedd1120560f13db683da8095823aa
mrtampan <mrtampan54@gmail.com>
2024-01-11 10:16:04 +0700

    logincontroller create token sso and remove sso

--- auth-app/app/Http/Controllers/Auth/LoginController.php
+++ auth-app/app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 
 class LoginController extends Controller
 {
@@ -37,5 +39,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function authenticated(Request $request, $user){
+        $token = $user->createToken('ssotoken')->plainTextToken;
+        Cookie::queue('ssotoken', $token, 60 * 24 * 7, null, 'localhost');
+
+        if ($request->redirect_url) {
+
+            return redirect($request->redirect_url . '/sso/' . $token);
+        }
     }
 }
